22c27d3bc4f16a5279e4b57a91b4f9b70d68607f
kangugi <muthama3897@gmail.com>
2024-07-25 15:15:12 +0300

    update v1

--- toursfile.php
+++ toursfile.php
@@ -10,12 +10,12 @@
                     <ul class="nav nav-pills d-inline-flex justify-content-center mb-5">
                         <li class="nav-item">
                             <a class="d-flex mx-3 py-2 border border-primary bg-light rounded-pill active" data-bs-toggle="pill" href="#NationalTab-1">
-                                <span class="text-dark" style="width: 250px;">National Tour Category</span>
+                                <span class="text-dark" style="width: 250px;">Software Category</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="d-flex py-2 mx-3 border border-primary bg-light rounded-pill" data-bs-toggle="pill" href="#InternationalTab-2">
-                                <span class="text-dark" style="width: 250px;">International tour Category</span>
+                                <span class="text-dark" style="width: 250px;">Hardware Category</span>
                             </a>
                         </li>
                     </ul>
@@ -27,8 +27,8 @@
                                         <img src="img/explore-tour-1.jpg" class="img-fluid w-100 rounded" alt="Image">
                                         <div class="national-content">
                                             <div class="national-info">
-                                                <h5 class="text-white text-uppercase mb-2">Weekend Tour</h5>
-                                                <a href="#" class="btn-hover text-white">View All Place <i class="fa fa-arrow-right ms-2"></i></a>
+                                                <h5 class="text-white text-uppercase mb-2">Consultants</h5>
+                                                <a href="#" class="btn-hover text-white">View All <i class="fa fa-arrow-right ms-2"></i></a>
                                             </div>
                                         </div>
                                         <div class="national-plus-icon">
@@ -41,7 +41,7 @@
                                         <img src="img/explore-tour-2.jpg" class="img-fluid w-100 rounded" alt="Image">
                                         <div class="national-content">
                                             <div class="national-info">
-                                                <h5 class="text-white text-uppercase mb-2">Holiday Tour</h5>
+                                                <h5 class="text-white text-uppercase mb-2">Insights</h5>
                                                 <a href="#" class="btn-hover text-white">View All Place <i class="fa fa-arrow-right ms-2"></i></a>
                                             </div>
                                         </div>
@@ -55,8 +55,8 @@
                                         <img src="img/explore-tour-3.jpg" class="img-fluid w-100 rounded" alt="Image">
                                         <div class="national-content">
                                             <div class="national-info">
-                                                <h5 class="text-white text-uppercase mb-2">Road Trip</h5>
-                                                <a href="#" class="btn-hover text-white">View All Place <i class="fa fa-arrow-right ms-2"></i></a>
+                                                <h5 class="text-white text-uppercase mb-2">Marketing</h5>
+                                                <a href="#" class="btn-hover text-white">View All<i class="fa fa-arrow-right ms-2"></i></a>
                                             </div>
                                         </div>
                                         <div class="tour-offer bg-info">15% Off</div>
@@ -70,7 +70,7 @@
                                         <img src="img/explore-tour-4.jpg" class="img-fluid w-100 rounded" alt="Image">
                                         <div class="national-content">
                                             <div class="national-info">
-                                                <h5 class="text-white text-uppercase mb-2">Historical Trip</h5>
+                                                <h5 class="text-white text-uppercase mb-2">Developers</h5>
                                                 <a href="#" class="btn-hover text-white">View All Place <i class="fa fa-arrow-right ms-2"></i></a>
                                             </div>
                                         </div>
@@ -84,7 +84,7 @@
                                         <img src="img/explore-tour-5.jpg" class="img-fluid w-100 rounded" alt="Image">
                                         <div class="national-content">
                                             <div class="national-info">
-                                                <h5 class="text-white text-uppercase mb-2">Family Tour</h5>
+                                                <h5 class="text-white text-uppercase mb-2">Infrastructure</h5>
                                                 <a href="#" class="btn-hover text-white">View All Place <i class="fa fa-arrow-right ms-2"></i></a>
                                             </div>
                                         </div>
@@ -99,7 +99,7 @@
                                         <img src="img/explore-tour-6.jpg" class="img-fluid w-100 rounded" alt="Image">
                                         <div class="national-content">
                                             <div class="national-info">
-                                                <h5 class="text-white text-uppercase mb-2">Beach Tour</h5>
+                                                <h5 class="text-white text-uppercase mb-2">Personalization</h5>
                                                 <a href="#" class="btn-hover text-white">View All Place <i class="fa fa-arrow-right ms-2"></i></a>
                                             </div>
                                         </div>
@@ -116,7 +116,7 @@
                                     <img src="img/explore-tour-1.jpg" class="img-fluid w-100 rounded" alt="Image">
                                     <div class="international-content">
                                         <div class="international-info">
-                                            <h5 class="text-white text-uppercase mb-2">Australia</h5>
+                                            <h5 class="text-white text-uppercase mb-2">Repairs</h5>
                                             <a href="#" class="btn-hover text-white me-4"><i class="fas fa-map-marker-alt me-1"></i> 8 Cities</a>
                                             <a href="#" class="btn-hover text-white"><i class="fa fa-eye ms-2"></i> <span>143+ Tour Places</span></a>
                                         </div>
@@ -130,7 +130,7 @@
                                     <img src="img/explore-tour-2.jpg" class="img-fluid w-100 rounded" alt="Image">
                                     <div class="international-content">
                                         <div class="international-info">
-                                            <h5 class="text-white text-uppercase mb-2">Germany</h5>
+                                            <h5 class="text-white text-uppercase mb-2">Hot Deals</h5>
                                             <a href="#" class="btn-hover text-white me-4"><i class="fas fa-map-marker-alt me-1"></i> 12 Cities</a>
                                             <a href="#" class="btn-hover text-white"><i class="fa fa-eye ms-2"></i> <span>21+ Tour Places</span></a>
                                         </div>
@@ -144,7 +144,7 @@
                                     <div class="international-content">
                                         <div class="tour-offer bg-warning">45% Off</div>
                                         <div class="international-info">
-                                            <h5 class="text-white text-uppercase mb-2">Spain</h5>
+                                            <h5 class="text-white text-uppercase mb-2">Special Offers</h5>
                                             <a href="#" class="btn-hover text-white me-4"><i class="fas fa-map-marker-alt me-1"></i> 9 Cities</a>
                                             <a href="#" class="btn-hover text-white"><i class="fa fa-eye ms-2"></i> <span>133+ Tour Places</span></a>
                                         </div>
@@ -157,7 +157,7 @@
                                     <img src="img/explore-tour-4.jpg" class="img-fluid w-100 rounded" alt="Image">
                                     <div class="international-content">
                                         <div class="international-info">
-                                            <h5 class="text-white text-uppercase mb-2">Japan</h5>
+                                            <h5 class="text-white text-uppercase mb-2">Trainings</h5>
                                             <a href="#" class="btn-hover text-white me-4"><i class="fas fa-map-marker-alt me-1"></i> 8 Cities</a>
                                             <a href="#" class="btn-hover text-white"><i class="fa fa-eye ms-2"></i> <span>137+ Tour Places</span></a>
                                         </div>
@@ -171,7 +171,7 @@
                                     <div class="international-content">
                                         <div class="tour-offer bg-info">70% Off</div>
                                         <div class="international-info">
-                                            <h5 class="text-white text-uppercase mb-2">London</h5>
+                                            <h5 class="text-white text-uppercase mb-2">Tutorials</h5>
                                             <a href="#" class="btn-hover text-white me-4"><i class="fas fa-map-marker-alt me-1"></i> 17 Cities</a>
                                             <a href="#" class="btn-hover text-white"><i class="fa fa-eye ms-2"></i> <span>26+ Tour Places</span></a>
                                         </div>
