09dbbb36cc811dc0f46f7c30ee580445d3f15ec1
kangugi <muthama3897@gmail.com>
2024-07-30 12:13:26 +0300

    updates

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title>Prestige Digital Hubs</title>
+        <title>Prestige Digital Hub</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
