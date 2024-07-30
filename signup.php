6a98b156748da69f6ce3debb0e8bfe6308100649
kangugi <muthama3897@gmail.com>
2024-07-30 14:20:18 +0300

    updates: update

---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prestige Digital Hub</title>
+        <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    </head>
+    <body>
+        <div class="registration-form">
+            <h1>Register Here</h1>
+            <form action="#" method="post">
+                <p>Full Name</p>
+                <input type="text" name="fullname" placeholder="Full Name">
+                <p>User Name</p>
+                <input type="text" name="username" placeholder="Username">
+                <p>Phone Number</p>
+                <input type="text" name="phone" placeholder="Mobile Number">
+                <p>Email:</p>
+                <input type="email" name="email" placeholder="Email Address">
+                <p>Password</p>
+                <input type="password" name="password" placeholder="Password">
+                <p>Confirm Password</p>
+                <input type="password" name="password" placeholder="Confirm Password">
+                <button type="submit">Register</button>
+
+            </form>
+        </div>
+    </body>
+</html>
